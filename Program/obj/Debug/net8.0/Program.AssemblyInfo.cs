9c4c1164d80c2be967df4bec035f028af25c53bd
JoacoGar <joaquins.garcia@correo.ucu.edu.uy>
2024-10-30 09:58:06 -0300

    Actualización

--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c106d6af3be95c765d1fac36f5a7feba5efbd8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eec3640dd57ada555d9d3484f43051001163001")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
