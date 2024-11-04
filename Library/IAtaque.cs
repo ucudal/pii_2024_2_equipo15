c6c71490dcae8be63e934ce942b83562e299fc29
JoacoGar <joaquins.garcia@correo.ucu.edu.uy>
2024-11-04 13:52:49 -0300

    commit para resetear

--- Library/IAtaque.cs
+++
@@ -1,8 +0,0 @@
-namespace Library;
-
-public interface IAtaque
-{
-    string Nombre { get; set; } // Nombre del ataque 
-    int Daño { get; set; } //Daño del ataque 
-    ITipo Tipo { get; set; } // El tipo del ataque 
-}
