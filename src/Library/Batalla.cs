4dee8b94a67830c4d356afc5de1ee7f7fedbbde8
Joaco Balian <joaquin.balian@correo.ucu.edu.uy>
2024-11-08 11:39:42 -0300

    Conocer ganador

--- src/Library/Batalla.cs
+++ src/Library/Batalla.cs
@@ -18,9 +18,40 @@ public class Batalla
     
     public void ConocerGanador()
     {
-        
+        {
+            bool TodosSinSaludEntrenador1 = true;
+            bool TodosSinSaludEntrenador2 = true;
+
+            foreach (Pokemon pokemon in Entrenador1.Equipo)  //Buscar si hay algún pokemon de Entrenador1 que tenga Salud
+            {
+                if (pokemon.Salud > 0)  //Si encontramos un pokemon que su Ssalud sea mayor que 0
+                {
+                    TodosSinSaludEntrenador1 = false;
+                    break; 
+                }
+            }
+
+            foreach (var pokemon in Entrenador2.Equipo) //Buscar si hay algún pokemon de Entrenador2 que tenga Salud
+            {
+                if (pokemon.Salud > 0) //Si encontramos un pokemon que su Salud sea mayor que 0
+                {
+                    TodosSinSaludEntrenador2 = false;
+                    break;  
+                }
+            }
+
+            if (TodosSinSaludEntrenador1) //Si uno de los dos equipos tiene a todos sus pokemons con Salud=0
+            {
+                Console.WriteLine( $"El equipo de {Entrenador2.Nombre} ha ganado"); 
+            }
+            else if (TodosSinSaludEntrenador2)
+            {
+                Console.WriteLine($"El equipo de {Entrenador1.Nombre} ha ganado"); 
+            }
+            
+        }
+
     }
-    
     public void PokemonDebilitado()
     {
         
