bbb597e26cf4834ebec720a040b4781508a0320d
Nicolas Perez <nicolas.pgarcia@correo.ucu.edu.uy>
2024-11-18 13:10:01 -0300

    Corrección de test e implementación de XML

---
+++ test/LibraryTests/AtaquesTest.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+namespace LibraryTests;
+using Library;
+
+public class AtaquesTest
+{
+    /// <summary>
+    /// Verifica que los ataques se inicialicen correctamente y que sus propiedades sean válidas.
+    /// </summary>
+    [Test]
+    public void Ataques()
+    {
+        // Creamos un ataque de tipo Fuego
+        var tipo_fuego = new Tipos("Fuego");
+        var ataqueLanzallama = new Ataques("Lanzallamas", 90, tipo_fuego, true);
+
+        // Creamos un ataque de tipo Volador
+        var tipo_volador = new Tipos("Volador");
+        var ataqueAcero = new Ataques("AlaDeAcero", 70, tipo_volador, false);
+
+        // Creamos un ataque de tipo Veneno
+        var tipo_veneno = new Tipos("Veneno");
+        var ataqueMugre = new Ataques("LanzaMugre", 65, tipo_veneno, false);
+
+        // Creamos de un ataque de tipo Psíquico
+        var tipo_psiquico = new Tipos("Psiquico");
+        var ataqueConfusion = new Ataques("Confusion", 50, tipo_psiquico, true);
+
+        // Validacion del ataque de tipo Fuego
+        Assert.That(ataqueLanzallama.Nombre, Is.EqualTo("Lanzallamas"));
+        Assert.That(ataqueLanzallama.Daño, Is.EqualTo(90));
+        Assert.That(ataqueLanzallama.Tipo.NombreTipo, Is.EqualTo("Fuego"));
+        Assert.That(ataqueLanzallama.EsEspecial, Is.True);
+
+        // Validacion del ataque de tipo Volador
+        Assert.That(ataqueAcero.Nombre, Is.EqualTo("AlaDeAcero"));
+        Assert.That(ataqueAcero.Daño, Is.EqualTo(70));
+        Assert.That(ataqueAcero.Tipo.NombreTipo, Is.EqualTo("Volador"));
+        Assert.That(ataqueAcero.EsEspecial, Is.False);
+
+        // Validacion del ataque de tipo Veneno
+        Assert.That(ataqueMugre.Nombre, Is.EqualTo("LanzaMugre"));
+        Assert.That(ataqueMugre.Daño, Is.EqualTo(65));
+        Assert.That(ataqueMugre.Tipo.NombreTipo, Is.EqualTo("Veneno"));
+        Assert.That(ataqueMugre.EsEspecial, Is.False);
+
+        // Validacion del ataque de tipo Psíquico
+        Assert.That(ataqueConfusion.Nombre, Is.EqualTo("Confusion"));
+        Assert.That(ataqueConfusion.Daño, Is.EqualTo(50));
+        Assert.That(ataqueConfusion.Tipo.NombreTipo, Is.EqualTo("Psiquico"));
+        Assert.That(ataqueConfusion.EsEspecial, Is.True);
+    }
+}
---
+++ test/LibraryTests/BatallaTest.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+namespace LibraryTests;
+using Library;
+
+public class BatallaTest
+{
+    /// <summary>
+    /// Válida que los entrenadores inicializados en la batalla sean los esperados.
+    /// Comprueba que las propiedades <see cref="Batalla.Entrenador1"/> y <see cref="Batalla.Entrenador2"/> 
+    /// correspondan a los valores dados durante la creación de la batalla.
+    /// </summary>
+    [Test]
+    public void Batalla()
+    {
+        var entrenador1 = new Entrenador("Juan");
+        var entrenador2 = new Entrenador("Mateo");
+        var batalla = new Batalla(entrenador1, entrenador2);
+
+        Assert.That(batalla.Entrenador1.Nombre, Is.EqualTo("Juan"));
+        Assert.That(batalla.Entrenador2.Nombre, Is.EqualTo("Mateo"));
+    }
+
+    /// <summary>
+    /// Válida el cambio de turno en la batalla.
+    /// Comprueba que el turno actual cambia después de llamar al método <see cref="Batalla.CambiarTurno"/>.
+    /// </summary>
+    [Test]
+    public void CambiarTurno()
+    {
+        var entrenador1 = new Entrenador("Juan");
+        var entrenador2 = new Entrenador("Mateo");
+        var batalla = new Batalla(entrenador1, entrenador2);
+
+        var turnoInicial = batalla.MostrarTurnoActual();
+        batalla.CambiarTurno();
+        var turnoFinal = batalla.MostrarTurnoActual();
+
+        Assert.That(turnoInicial, Is.Not.EqualTo(turnoFinal));
+    }
+
+    /// <summary>
+    /// Válida que un entrenador pueda usar un ítem durante su turno.
+    /// Comprueba que el mensaje devuelto por el método <see cref="Batalla.UsarItem"/> sea correcto.
+    /// </summary>
+    [Test]
+    public void UsarItem()
+    {
+        var entrenador1 = new Entrenador("Juan");
+        var entrenador2 = new Entrenador("Mateo");
+        var batalla = new Batalla(entrenador1, entrenador2);
+
+        var resultado = batalla.UsarItem(entrenador1, "SuperPocion");
+
+        Assert.That(resultado, Contains.Substring("Uso un item y pierde el turno"));
+    }
+
+    /// <summary>
+    /// Válida que el método <see cref="Batalla.ConocerGanador"/> devuelva el entrenador ganador
+    /// cuando todos los Pokémon de uno de los entrenadores están debilitados.
+    /// </summary>
+    [Test]
+    public void ConocerGanador()
+    {
+        var entrenador1 = new Entrenador("Juan");
+        var entrenador2 = new Entrenador("Mateo");
+        var batalla = new Batalla(entrenador1, entrenador2);
+
+        // Se debilitan todos los Pokémon del entrenador1
+        foreach (var pokemon in entrenador1.Equipo)
+        {
+            pokemon.Salud = 0;
+        }
+
+        var ganador = batalla.ConocerGanador();
+
+        Assert.That(ganador, Contains.Substring("El equipo de Mateo es el ganador"));
+    }
+
+    /// <summary>
+    /// Válida que el método <see cref="Batalla.PokemonDebilitado"/> identifique correctamente
+    /// si un Pokémon ha sido derrotado en la batalla.
+    /// </summary>
+    [Test]
+    public void PokemonDebilitado()
+    {
+        var entrenador1 = new Entrenador("Juan");
+        var entrenador2 = new Entrenador("Mateo");
+        var batalla = new Batalla(entrenador1, entrenador2);
+
+        // Se debilita un Pokémon del entrenador1
+        entrenador1.Equipo[0].Salud = 0;
+
+        var resultado = batalla.PokemonDebilitado();
+
+        Assert.That(resultado, Contains.Substring("no puede continuar peleando"));
+    }
+}
