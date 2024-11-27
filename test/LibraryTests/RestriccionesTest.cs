798e84bc9b79367a2de3b44a78ceb155c1dbf467
JoacoGar <joaquins.garcia@correo.ucu.edu.uy>
2024-11-27 12:18:07 -0300

    Se termina los test
    No logre implementar en la logica que los tipos se pudiesen restringir, es decir en la clase restriccion si, pero en el flujo de batalla no.

--- test/LibraryTests/RestriccionesTest.cs
+++ test/LibraryTests/RestriccionesTest.cs
@@ -3,22 +3,49 @@ using program;
 
 namespace LibraryTests;
 
+[TestFixture]
 public class RestriccionesTest
 {
-    private Restricciones items;
-    private Restricciones pokemon;
-    private Restricciones tipos;
+    private Restricciones restricciones;
     
     [SetUp]
     public void SetUp()
     {
-        items = new Restricciones();
-        pokemon = new Restricciones();
-        tipos = new Restricciones();
+        restricciones = new Restricciones();
     }
-    
+
     [Test]
-    public void 
+    public void RestringirItems_AgregaItemsALista()
+    {
+        string resultado = restricciones.RestringirItems("Súper poción");
+        Assert.That(resultado, Does.Contain("Súper poción"));
+    }
+    [Test]
+    public void RestringirTipos_AgregaTiposALista()
+    {
+        string resultado = restricciones.RestringirItems("Fuego");
+        Assert.That(resultado, Does.Contain("Fuego"));
+    }
+    [Test]
+    public void RestringirPokemon_AgregaPokemonALista()
+    {
+        string resultado = restricciones.RestringirItems("Pikachu");
+        Assert.That(resultado, Does.Contain("Pikachu"));
+    }
+
+    [Test]
+    public void AceptarRestricciones_DecisionSi()
+    {
+        Restricciones.AceptarRestricciones("si");
+        Assert.That(Restricciones.AceptacionRestriccion, Is.EqualTo(true));
+    }
+
+    [Test]
+    public void AceptarRestricciones_DecisionNo()
+    {
+        Restricciones.AceptarRestricciones("no");
+        Assert.That(Restricciones.AceptacionRestriccion, Is.EqualTo(false));
+    }
 
 }
 
